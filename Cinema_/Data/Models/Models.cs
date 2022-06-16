af92b8d8b9af23caf84cc4a339d7bb83c7d6a786
kyrylo <kyrylo.bondarenko@nure.ua>
2022-06-16 14:19:02 +0300

    auth

---
+++ Cinema_/Data/Models/Models.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema.Data.Models
+{
+    public class LoginModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+    public class RegisterModel
+    {
+        public string Login { set; get; }
+        public string Password { get; set; }
+
+        
+    }
+}
