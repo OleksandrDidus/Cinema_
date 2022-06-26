5838f1b585519962a9d635b6abbb9d1fd8b69051
Oleksandr Didus <oleksandr.didus@nure.ua>
2022-06-26 18:25:00 +0300

    123

--- Cinema_/Controllers/TicketController.cs
+++ Cinema_/Controllers/TicketController.cs
@@ -1,4 +1,5 @@
-﻿using Cinema.Data.Models;
+﻿using Cinema.Data;
+using Cinema.Data.Models;
 using Cinema.Data.Repository;
 using Cinema.ViewModels;
 using Microsoft.AspNetCore.Authorization;
@@ -21,15 +22,20 @@ namespace Cinema.Controllers
         private readonly IGenericRepository<Place> _placeRepository;
         private readonly IGenericRepository<Ticket> _ticketRepository;
         private readonly IGenericRepository<User> _userManager;
+        private readonly CinemaContext _dbcontext;
+
+        
 
         public TicketController(IGenericRepository<Session> sessionRepository, IGenericRepository<Place> placeRepository,
             IGenericRepository<Ticket> ticketRepository,
-            IGenericRepository<User> userManager)
+            IGenericRepository<User> userManager,
+            CinemaContext dbcontext)
         {
             _sessionRepository = sessionRepository;
             _placeRepository = placeRepository;
             _userManager = userManager;
             _ticketRepository = ticketRepository;
+            this._dbcontext = dbcontext;
         }
 
         // Method to generate ticket purchase page
@@ -114,6 +120,54 @@ namespace Cinema.Controllers
             }
 
             return sb.ToString();
+        }
+        [HttpPost]
+        public async Task<ActionResult> CheckCode(string bookCode)
+        {
+            //var ticket = (await _ticketRepository.GetAsync(u => u.BookingCode == altitudeString,
+            //    i =>i.Include(x=>x.Session).ThenInclude(x=>x.Film))).FirstOrDefault();
+
+            var ticker = await this._dbcontext.Tickets.FirstOrDefaultAsync(i => i.BookingCode == bookCode);
+            if(ticker is not null)
+            {
+                ticker.IsPaid = true;
+                ticker.IsDestroyed = true;
+                await this._dbcontext.SaveChangesAsync();
+                return Content("Код вірний, квиток надруковано!");
+            }
+            else
+            {
+                return Content("Такого коду не існує!");
+            }
+            //model.Session = (await _sessionRepository.GetAsync(q => q.Id == id,
+            //i => i.Include(x => x.Film)
+            ////      .Include(x => x.Tickets))).FirstOrDefault();
+            //model.Session = (await _sessionRepository.GetAsync(q => q.Id == id,
+            //    i => i.Include(x => x.Film)
+            //          .Include(x => x.Tickets))).FirstOrDefault();
+            //Ticket ticket1 = new Ticket()
+            //{
+            //    Id = ticket.Id,
+            //    IsDestroyed = ticket.IsDestroyed,
+            //    SessionId = ticket.SessionId,
+            //    IsPaid = ticket.IsPaid,
+            //    Price = ticket.Price,
+            //    PlaceId = ticket.PlaceId,
+            //    UserId = (await _userManager.GetAsync(q => q.Login == User.Identity.Name))
+            //         .FirstOrDefault().Id,
+            //    BookingCode = ticket.BookingCode,
+            //    SoldTicketQRCode = ticket.SoldTicketQRCode
+            //};
+            //if (ticket == null)
+            //{
+            //    return Content("Такого коду не існує!");
+            //}
+            //else
+            //{
+            //    ticket.IsPaid = true;
+            //    ticket.IsDestroyed = true;
+            //    return Content("Код вірний, квиток надруковано!");
+            //}
         }
 
         // GET: TicketController/Edit/5
